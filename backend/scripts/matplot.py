2b1fa1e8f14d90c3d2a77e0a123bbb25066585eb
BenjaminOReb <benjamin.ortega1801@alumnos.ubiobio.cl>
2025-07-30 16:35:57 -0400

    Ajustes a grafico, etiquetas y colores

--- backend/scripts/matplot.py
+++ backend/scripts/matplot.py
@@ -6,18 +6,22 @@ import numpy as np
 # --- Configuración de archivos y variables ---
 # Ajusta estas rutas si tus archivos están en otras carpetas
 files = [
-    ("uploads/riesgo_raw/riesgo_raw_2019-05.nc", "riesgo_raw",    "Riesgo Crudo"),
+    ("uploads/riesgo_raw/riesgo_raw_2019-05.nc", "riesgo_raw",    "Riesgo Crisp"),
     ("uploads/riesgo_fuzzy/riesgo_fuzzy_2019-05.nc", "riesgo_fuzzy", "Riesgo Fuzzy"),
-    ("uploads/recortado/t2m_2019-05_recortado.nc",   "t2m",          "Temperatura Cruda (t2m)"),
-    ("uploads/fuzzy/fuzzy_t2m_2019-05.nc",           "t2m_alta",    "Temperatura Fuzzy (alta)"),
-    ("uploads/recortado/pr_2019-05_recortado.nc",    "pr",           "Precipitación Cruda (pr)"),
-    ("uploads/fuzzy/fuzzy_pr_2019-05.nc",            "pr_baja",     "Precipitación Fuzzy (baja)")
+    #("uploads/recortado/t2m_2019-05_recortado.nc",   "t2m",          "Temperatura"),
+    #("uploads/fuzzy/fuzzy_t2m_2019-05.nc",           "t2m_baja",    "Temperatura Fuzzy (baja)"),
+    #("uploads/fuzzy/fuzzy_t2m_2019-05.nc",           "t2m_media",    "Temperatura Fuzzy (moderada)"),
+    #("uploads/fuzzy/fuzzy_t2m_2019-05.nc",           "t2m_alta",    "Temperatura Fuzzy (alta)"),
+    #("uploads/recortado/pr_2019-05_recortado.nc",    "pr",           "Precipitación"),
+    #("uploads/fuzzy/fuzzy_pr_2019-05.nc",            "pr_baja",     "Precipitación Fuzzy (baja)"),
+    #("uploads/fuzzy/fuzzy_pr_2019-05.nc",            "pr_media",     "Precipitación Fuzzy (moderada)"),
+    #("uploads/fuzzy/fuzzy_pr_2019-05.nc",            "pr_alta",     "Precipitación Fuzzy (alta)")
 ]
 
-time_index = 55 # índice temporal a plotear (0 a 59 en tu caso)
+time_index = 59 # índice temporal a plotear (0 a 59 en tu caso)
 
 # --- Crear figura con 2 filas x 3 columnas ---
-fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(18, 10), 
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(18, 10), 
                          subplot_kw={"projection": None})
 axes = axes.flatten()
 
@@ -44,7 +48,7 @@ for ax, (ruta, var, titulo) in zip(axes, files):
     #    Creamos la malla de coordenadas
     Lon, Lat = np.meshgrid(lons, lats)
     pcm = ax.pcolormesh(Lon, Lat, data,
-                        cmap="viridis", shading="auto")
+                        cmap="plasma", shading="auto")
     
     ax.set_title(titulo, fontsize=14)
     ax.set_xlabel("Longitud")
